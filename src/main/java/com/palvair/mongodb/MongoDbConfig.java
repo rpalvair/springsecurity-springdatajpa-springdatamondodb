3f25eb275107cb92ecec359a93bf8f295dd52179
widdy <r.palvair@gmail.com>
2016-02-07 17:01:39 +0100

    Externalize database name in application.properties

--- src/main/java/com/palvair/mongodb/MongoDbConfig.java
+++ src/main/java/com/palvair/mongodb/MongoDbConfig.java
@@ -2,6 +2,7 @@ package com.palvair.mongodb;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,9 +14,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
  */
 @Configuration
 public class MongoDbConfig extends AbstractMongoConfiguration {
+    @Value("${db.name}")
+    private String dbName;
+
     @Override
     protected String getDatabaseName() {
-        return "rpalvair";
+        return dbName;
     }
 
     @Override
